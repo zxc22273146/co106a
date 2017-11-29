d4d95c81c54a4e62abcff9cc9820924c9776b2e6
zxc22273146 <zxc22273146@gmail.com>
2017-11-29 22:16:53 +0800

    finish 04

--- exercise/04/fill/Fill.asm
+++ exercise/04/fill/Fill.asm
@@ -12,3 +12,67 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+//forever
+//  arr = SCREEN
+//  for (i=0; i<8192; i++) {
+//    if (*KBD != 0)
+//      arr[i] = -1
+//    else
+//      arr[i] = 0
+//  }
+//  goto forever
+
+(RESTART)
+@SCREEN
+D=A
+@0
+M=D	//PUT SCREEN START LOCATION IN RAM0
+
+///////////////////////////
+(KBDCHECK)
+
+@KBD
+D=M
+@BLACK
+D;JGT	//JUMP IF ANY KBD KEYS ARE PRESSED
+@WHITE
+D;JEQ	//ELSE JUMP TO WHITEN
+
+@KBDCHECK
+0;JMP
+///////////////////////////
+(BLACK)
+@1
+M=-1	//WHAT TO FILL SCREEN WITH (-1=11111111111111)
+@CHANGE
+0;JMP
+
+(WHITE)
+@1
+M=0	//WHAT TO FILL SCREEN WITH
+@CHANGE
+0;JMP
+//////////////////////////
+(CHANGE)
+@1	//CHECK WHAT TO FILL SCREEN WITH
+D=M	//D CONTAINS BLACK OR WHITE
+
+@0
+A=M	//GET ADRESS OF SCREEN PIXEL TO FILL
+M=D	//FILL IT
+
+@0
+D=M+1	//INC TO NEXT PIXEL
+@KBD
+D=A-D	//KBD-SCREEN=A
+
+@0
+M=M+1	//INC TO NEXT PIXEL
+A=M
+
+@CHANGE
+D;JGT	//IF A=0 EXIT AS THE WHOLE SCREEN IS BLACK
+/////////////////////////
+@RESTART
+0;JMP
--- exercise/04/mult/mult.asm
+++ exercise/04/mult/mult.asm
@@ -7,3 +7,48 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//a = 0
+//LOOP:
+//  if (a <= 0) goto EXIT
+//    a=a-1
+//  R2 = R2 + R1
+//  goto LOOP
+
+@2	//GO TO FINAL ANSWER BOX
+M=0	//ZERO ANS BOX
+
+@0
+D=M
+@END
+D;JEQ	//IF ONE PRODUCT IS ZERO
+
+@1
+D=M
+@END
+D;JEQ	//IF ONE PRODUCT IS ZERO
+
+@0	//NOT NECESSARY
+D=M	//
+@3	//
+M=D	//ONLY TO KEEP THE NUMBERS BEING MUTLIPLED
+
+
+(LOOP)
+@1	//GET 2ND NUM
+D=M	//D HAS 2ND NUM
+
+@2	//GO TO FINAL ANSWER BOX
+M=D+M	//RAM[2] NOW HAS 2ND NUMBER + ITS PREVIOUS VALUE
+
+@3	//GET 1ST NUM
+M=M-1	//1ST NUM-1
+
+D=M	//IDK WHY D NEEDS TO =M?
+@LOOP	//WHERE TO JUMP TO
+D;JGT	//JUMP		    (WHY CANT THIS BE M;JGT?)
+
+
+(END)
+@END
+0;JMP	//FOREVER LOOP
